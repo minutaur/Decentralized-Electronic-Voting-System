eb7b7721b467739c0f2a5065a9b1bfd8770324b6
minutaur <95672180+minutaur@users.noreply.github.com>
2022-08-20 16:18:16 +0900

    Update 02.rest_api.py

--- lab_server/02.rest_api.py
+++ lab_server/02.rest_api.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/api1', methods=['GET'])
 def f1():
-    return 'GET api1'
+    return jsonify({'status': 'success'})
 
 
 @app.route('/api2', methods=['POST'])
 def f2():
-    return 'POST api2'
+    data = request.get_json()
+    return jsonify(data)
 
 
 app.run()

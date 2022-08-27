99f15c6c2005095e4171c5772b1255114dced490
minutaur <95672180+minutaur@users.noreply.github.com>
2022-08-27 16:34:26 +0900

    Update 02.py

--- Quiz/02.py
+++ Quiz/02.py
@@ -35,7 +35,19 @@ while True:
 
 
     elif menu == '3':
-        pass
+        headers = {'Content-Type': 'application/json'}
+
+        id = input('투표ID: ')
+        vote = input('투표항목: ')
+
+        data = {
+            'id': id,
+            'vote': vote
+        }
+
+        res = requests.post('http://127.0.0.1:5000/vote', data=json.dumps(data), headers=headers)
+
+        print(res.text)
 
     elif menu == '4':
         break
